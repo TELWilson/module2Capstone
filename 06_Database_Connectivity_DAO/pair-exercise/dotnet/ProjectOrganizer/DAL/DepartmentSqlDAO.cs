42d7cd86da17a32347c7d07520d40826b87a9567
Leslie <leslies.email3@gmail.com>
2020-10-19 14:35:52 -0400

    start of day 1 week 6 pair exercise

--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/DepartmentSqlDAO.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace ProjectOrganizer.DAL
 {
     public class DepartmentSqlDAO : IDepartmentDAO
     {
         private string connectionString;
+
+        private string sqlGetDepartment = "SELECT * FROM department;";
 
         // Single Parameter Constructor
         public DepartmentSqlDAO(string dbConnectionString)
@@ -23,7 +27,33 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            IList<Department> departments = new List<Department>();
+
+            // Create the SQL connection
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Open the SQL connection
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlGetDepartment, conn);
+
+                // Create a reader
+                // An exception can be thrown here. IE: Connection string is bad
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Create a new instance of department every loop (every line)
+                    Department department = new Department();
+
+                    department.Id = Convert.ToInt32(reader["department_id"]);
+                    department.Name = Convert.ToString(reader["name"]);
+
+                    departments.Add(department);
+                }
+            }
+
+            return departments;
         }
 
         /// <summary>
--- 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/EmployeeSqlDAO.cs
+++ 06_Database_Connectivity_DAO/pair-exercise/dotnet/ProjectOrganizer/DAL/EmployeeSqlDAO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ProjectOrganizer.DAL
 {
@@ -11,6 +12,15 @@ namespace ProjectOrganizer.DAL
     {
         private string connectionString;
 
+        // SQL Command to get all employees from the employees table
+        private string sqlGetAllEmployees = "SELECT * FROM employee;";
+
+        // SQL command to get all employees with the given firstname and lastname
+        private string sqlSearch = "SELECT * FROM employee WHERE employee.first_name = @first_name AND employee.last_name = @last_name;";
+
+        // SQL command to get all employees without a project
+        private string sqlGetEmployeesWithoutProjects = "SELECT * FROM employee JOIN project_employee ON project_employee.employee_id = employee_id JOIN project ON project.project_id = project_employee.project_id WHERE project_employee.project_id IS NULL;";
+        
         // Single Parameter Constructor
         public EmployeeSqlDAO(string dbConnectionString)
         {
@@ -23,7 +33,41 @@ namespace ProjectOrganizer.DAL
         /// <returns>A list of all employees.</returns>
         public IList<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            IList<Employee> employees = new List<Employee>();
+
+            // Create the SQL connection
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Open the SQL connection
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlGetAllEmployees, conn);
+
+                // Create a reader
+                // An exception can be thrown here. IE: Connection string is bad
+                SqlDataReader reader = command.ExecuteReader();
+
+                // While the reader has a row to read...
+                while (reader.Read())
+                {
+                    // Create a new instance of department every loop (every line)
+                    Employee employee = new Employee();
+
+                    employee.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+                    employee.DepartmentId = Convert.ToInt32(reader["department_id"]);
+                    employee.FirstName = Convert.ToString(reader["first_name"]);
+                    employee.LastName = Convert.ToString(reader["last_name"]);
+                    employee.JobTitle = Convert.ToString(reader["job_title"]);
+                    employee.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+                    employee.Gender = Convert.ToString(reader["gender"]);
+                    employee.HireDate = Convert.ToDateTime(reader["hire_date"]);
+                    
+
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
         }
 
         /// <summary>
@@ -35,7 +79,48 @@ namespace ProjectOrganizer.DAL
         /// <returns>A list of employees that match the search.</returns>
         public IList<Employee> Search(string firstname, string lastname)
         {
-            throw new NotImplementedException();
+            IList<Employee> employees = new List<Employee>();
+
+            // Create the SQL connection
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Open the SQL connection
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlSearch, conn);
+
+                // We passed a string firstname and lastname to the method
+                // We can call the parameters that we need from the passed strings
+                // (<name of our variable>, <the value>)
+                command.Parameters.AddWithValue("@first_name", firstname);
+                command.Parameters.AddWithValue("@last_name", lastname);
+
+
+                // Create a reader
+                // An exception can be thrown here. IE: Connection string is bad
+                SqlDataReader reader = command.ExecuteReader();
+
+                // While the reader has a row to read...
+                while (reader.Read())
+                {
+                    // Create a new instance of department every loop (every line)
+                    Employee employee = new Employee();
+
+                    employee.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+                    employee.DepartmentId = Convert.ToInt32(reader["department_id"]);
+                    employee.FirstName = Convert.ToString(reader["first_name"]);
+                    employee.LastName = Convert.ToString(reader["last_name"]);
+                    employee.JobTitle = Convert.ToString(reader["job_title"]);
+                    employee.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+                    employee.Gender = Convert.ToString(reader["gender"]);
+                    employee.HireDate = Convert.ToDateTime(reader["hire_date"]);
+
+
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
         }
 
         /// <summary>
@@ -44,7 +129,48 @@ namespace ProjectOrganizer.DAL
         /// <returns></returns>
         public IList<Employee> GetEmployeesWithoutProjects()
         {
-            throw new NotImplementedException();
+            IList<Employee> employees = new List<Employee>();
+
+            // Create the SQL connection
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Open the SQL connection
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlGetEmployeesWithoutProjects, conn);
+
+                // We passed a string firstname and lastname to the method
+                // We can call the parameters that we need from the passed strings
+                // (<name of our variable>, <the value>)
+                command.Parameters.AddWithValue("@first_name", firstname);
+                command.Parameters.AddWithValue("@last_name", lastname);
+
+
+                // Create a reader
+                // An exception can be thrown here. IE: Connection string is bad
+                SqlDataReader reader = command.ExecuteReader();
+
+                // While the reader has a row to read...
+                while (reader.Read())
+                {
+                    // Create a new instance of department every loop (every line)
+                    Employee employee = new Employee();
+
+                    employee.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+                    employee.DepartmentId = Convert.ToInt32(reader["department_id"]);
+                    employee.FirstName = Convert.ToString(reader["first_name"]);
+                    employee.LastName = Convert.ToString(reader["last_name"]);
+                    employee.JobTitle = Convert.ToString(reader["job_title"]);
+                    employee.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+                    employee.Gender = Convert.ToString(reader["gender"]);
+                    employee.HireDate = Convert.ToDateTime(reader["hire_date"]);
+
+
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
         }
     }
 }
